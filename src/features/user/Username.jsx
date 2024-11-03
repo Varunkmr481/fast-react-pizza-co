5c611009ecf874296d64acd43770694c8d613ccb
Varunkmr481 <kmrvarun481@gmail.com>
2024-11-03 21:02:09 +0530

    model user State with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Varun</div>;
+  // 'userName' state extraction
+  const userName = useSelector((state) => state.user.username);
+  console.log(userName);
+
+  if (!userName) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default Username;
